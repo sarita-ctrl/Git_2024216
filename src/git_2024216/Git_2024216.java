105e935b642e3c144d779ec5e7c4ac2472ed7419
sarita-ctrl <saritaarte@gmail.com>
2024-12-02 20:54:35 +0000

    Homework- Sample Git example

---
+++ src/git_2024216/Git_2024216.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024216;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Git_2024216 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         /*
+        This project is to demostrate the use of GitHub.
+        Adding a simple print in NetBeans and using git desktop to push the change to github
+        1.- Initialize the empty project in GitHub
+        2.- Publish it to GitHub. PUBLIC
+        3.- Add a line of code to your project with a simple output.
+        */
+    }
+    
+}
